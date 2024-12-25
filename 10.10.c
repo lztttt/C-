4c53987f9054c633fba65eae5e2031843de85df8
lztttt <lzt1975677818@gmail.com>
2024-12-25 11:52:12 +0800

    1

--- 10.10.c
+++ 10.10.c
@@ -1,10 +1,21 @@
+#include <stdio.h>
+int pdprim(int n)
+{
+    int i;
+    if (n <= 1)
+        return 0;
+    for (i = 2; i <= n / 2; i++)
+        if (n % i == 0)
+            return 0;
+    return 1;
+}
+// 
 int main()
 {
     int m;
-    scanf("%d",&m);
-    if(pdprim(m)==1) 
-		printf("素数\n");
+    scanf("%d", &m);
+    if (pdprim(m) == 1)
+        printf("素数\n");
     else
-		printf("不是素数\n");
+        printf("不是素数\n");
 }
-
