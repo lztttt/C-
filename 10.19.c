379fe5e0fce8ee6b8e888a8d3aa14b8f73625751
lztttt <lzt1975677818@gmail.com>
2024-12-25 16:34:15 +0800

    1

--- 10.19.c
+++ 10.19.c
@@ -2,11 +2,43 @@
 // 编程可能涉及数学函数的使用，fabs()函数功能：求实数绝对值
 #include <stdio.h>
 #include <math.h>
+double f(double x)
+{
+    return (x * x * x * x) - 6 * (x * x * x) + 3 * x * x - (10 * x) + 2;
+}
+double bisection(double a, double b)
+{
+    double mid;
+    while (fabs(a - b) > 1e-3)
+    {
+        mid = (a + b) / 2.0; // 计算中点
+        if (f(mid) == 0.0)
+        { // 找到根
+            return mid;
+        }
+        else if (f(mid) * f(a) < 0)
+        { // 根在 [a, mid] 区间
+            b = mid;
+        }
+        else
+        { // 根在 [mid, b] 区间
+            a = mid;
+        }
+    }
+    return a;
+}
 int main()
 {
-    double a, b, x1, x2, x3, x4, fx1, fx2, fx3, fx4, e = 1e-3;
-    scanf("%lf %lf", &a, &b);   
-
-    printf("x=%.5lf\n", x1);
+    double a, b, root;
+    // 从键盘输入区间 [a, b]
+    scanf("%lf %lf", &a, &b);
+    if (f(a) * f(b) >= 0)
+    {
+        printf("该区间无根\n");
+        return 1;
+    }
+    // 调用二分法
+    root = bisection(a, b);
+    printf("x=%.5lf\n", root);
     return 0;
 }
